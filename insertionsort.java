88fd9a5ac1aed356f6d4b07ee58701b19029080a
NC872b <112186989+NC872b@users.noreply.github.com>
2025-01-09 15:34:05 +0530

    Update Insertionsort.java

--- insertionsort.java
+++ insertionsort.java
@@ -1,15 +1,10 @@
 import java.util.Scanner;
-
-public class SimpleInsertionSort {
+public class Insertionsort {
     public static void insertionSort(int[] array) {
         int n = array.length;
-
         for (int i = 1; i < n; i++) {
             int key = array[i];
             int j = i - 1;
-
-            // Move elements of array[0..i-1], that are greater than key,
-            // to one position ahead of their current position
             while (j >= 0 && array[j] > key) {
                 array[j + 1] = array[j];
                 j--;
@@ -17,34 +12,26 @@ public class SimpleInsertionSort {
             array[j + 1] = key;
         }
     }
-
     public static void printArray(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
         }
         System.out.println();
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Enter the number of elements: ");
         int size = scanner.nextInt();
         int[] array = new int[size];
-
         System.out.println("Enter the elements:");
         for (int i = 0; i < size; i++) {
             array[i] = scanner.nextInt();
         }
-
         System.out.println("Original array:");
         printArray(array);
-
         insertionSort(array);
-
         System.out.println("Sorted array:");
         printArray(array);
-
         scanner.close();
     }
 }
